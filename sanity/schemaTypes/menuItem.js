79de6376588f68bd80cf7d4a8c8adf2fa28a5d87
DavidMoravec <David.Moravec2@seznam.cz>
2026-09-07 01:21:14 +0200

    refine Sanity menu item schema

--- sanity/schemaTypes/menuItem.js
+++ sanity/schemaTypes/menuItem.js
@@ -3,36 +3,43 @@ export default {
   title: "Menu item",
   type: "document",
   fields: [
-    { name: "title", title: "Title", type: "string" },
+    { name: "name", title: "Name", type: "string" },
+
     {
       name: "slug",
       title: "Slug",
       type: "slug",
-      options: { source: "title" },
+      options: { source: "name" },
     },
+
     {
       name: "category",
       title: "Category",
       type: "reference",
       to: [{ type: "menuCategory" }],
     },
+
     { name: "description", title: "Description", type: "text" },
     { name: "portion", title: "Portion", type: "string" },
     { name: "price", title: "Price (CZK)", type: "number" },
     { name: "vegetarian", title: "Vegetarian", type: "boolean" },
+
     {
       name: "allergens",
       title: "Allergens",
       type: "array",
-      of: [{ type: "string" }],
+      of: [{ type: "number" }],
     },
+
     {
       name: "image",
       title: "Image",
       type: "image",
       options: { hotspot: true },
     },
+
     { name: "imageAlt", title: "Image alternative text", type: "string" },
+
     {
       name: "variants",
       title: "Variants",
