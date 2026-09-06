906522ee86e10ba2f8bd0b7400dfedf9e4bba162
DavidMoravec <David.Moravec2@seznam.cz>
2026-09-07 01:28:01 +0200

    add Sanity gallery image schema

--- sanity/schemaTypes/index.js
+++ sanity/schemaTypes/index.js
@@ -2,10 +2,12 @@ import lunchMenu from "./lunchMenu";
 import menuCategory from "./menuCategory";
 import menuItem from "./menuItem";
 import restaurantSettings from "./restaurantSettings";
+import galleryImage from "./galleryImage";
 
 export const schemaTypes = [
   restaurantSettings,
   menuCategory,
   menuItem,
   lunchMenu,
+  galleryImage,
 ];
